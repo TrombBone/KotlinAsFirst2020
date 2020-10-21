e32e14abdc0566a0f127bb9e22be8a73c07ad2bc
trommbone <kruchinin-kostya@mail.ru>
2020-10-21 04:29:10 +0300

    Lesson 5 (with fixes) ready

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -312,11 +312,11 @@ fun bagPacking(treasures: Map<String, Pair<Int, Int>>, capacity: Int): Set<Strin
     val treasuresFit = mutableMapOf<String, Int>()
     val res = mutableSetOf<String>()
     for ((key, value) in treasures) if (value.first <= capacity) treasuresFit += (key to value.second / value.first)
-    val list = treasuresFit.values.sorted()
+    val list = treasuresFit.values.sortedBy { -it }
     val treasuresFitSort = mutableMapOf<String, Int>()
     for (element in list) treasuresFitSort += treasuresFit.filterValues { it == element }
     var engagedCapacity = 0
-    for ((key, _) in treasuresFit) {
+    for ((key, _) in treasuresFitSort) {
         engagedCapacity += treasures.getValue(key).first
         if (engagedCapacity > capacity) break
         res += key
