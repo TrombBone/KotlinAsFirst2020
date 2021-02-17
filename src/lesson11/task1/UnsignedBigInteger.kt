998883d1f3cc8c3e2871c253eb8b1ddc25edb316
kruch <kruchinin-kostya@mail.ru>
2021-02-17 22:43:21 +0300

    UnsignedBigInteger ready

--- src/lesson11/task1/UnsignedBigInteger.kt
+++ src/lesson11/task1/UnsignedBigInteger.kt
@@ -1,5 +1,7 @@
 package lesson11.task1
 
+import lesson3.task1.digitNumber
+
 /**
  * Класс "беззнаковое большое целое число".
  *
@@ -13,64 +15,195 @@ package lesson11.task1
  */
 class UnsignedBigInteger : Comparable<UnsignedBigInteger> {
 
+    private val unsignedBigInteger: MutableList<Int> = mutableListOf()
+
     /**
      * Конструктор из строки
      */
     constructor(s: String) {
-        TODO()
+        if (!s.matches(Regex("""\d+"""))) throw NumberFormatException("number contains unknown characters")
+        try {
+            unsignedBigInteger.add(s.toInt())
+        } catch (e: NumberFormatException) {
+            var postStr = Regex("""^(0+)""").replace(s, "")
+            while (postStr.isNotEmpty()) {
+                val startIntStr = try {
+                    postStr.substring(0, if (postStr.length > 10) 10 else postStr.length).toInt()
+                } catch (e: NumberFormatException) {
+                    postStr.substring(0, 9).toInt()
+                }
+                unsignedBigInteger.add(startIntStr)
+                if (digitNumber(startIntStr) - 1 == postStr.lastIndex) break
+                else postStr = postStr.substring(digitNumber(startIntStr), postStr.length)
+                while (postStr.matches(Regex("""0\d*"""))) {
+                    unsignedBigInteger.add(0)
+                    postStr = postStr.replaceFirst("0", "")
+                }
+            }
+        }
     }
 
     /**
      * Конструктор из целого
      */
     constructor(i: Int) {
-        TODO()
+        unsignedBigInteger.add(i)
     }
 
     /**
      * Сложение
      */
-    operator fun plus(other: UnsignedBigInteger): UnsignedBigInteger = TODO()
+    operator fun plus(other: UnsignedBigInteger): UnsignedBigInteger {
+//        println("this unsignedBigInteger: $this")
+//        println("other unsignedBigInteger: $other")
+        val biggerSize = maxOf(toString().length, other.toString().length)
+        val smallerSize = minOf(toString().length, other.toString().length)
+        var res = ""
+        var memory = 0
+        for (i in 1..biggerSize) {
+            val digit: Int = try {
+                getValue(biggerSize - 1).toString() //условие try, toString() для ориентации
+                getValue(biggerSize - i) + if (i >= smallerSize + 1) 0 else other.getValue(smallerSize - i)
+            } catch (e: StringIndexOutOfBoundsException) {
+                other.getValue(biggerSize - i) + if (i >= smallerSize + 1) 0 else getValue(smallerSize - i)
+            } + memory
+            memory = 0
+            if (digitNumber(digit) > 1) memory++
+            res = "${digit % 10}" + res
+        }
+        if (memory != 0) res = "1$res"
+        return UnsignedBigInteger(res)
+    }
 
     /**
      * Вычитание (бросить ArithmeticException, если this < other)
      */
-    operator fun minus(other: UnsignedBigInteger): UnsignedBigInteger = TODO()
+    operator fun minus(other: UnsignedBigInteger): UnsignedBigInteger {
+        if (this < other) throw ArithmeticException("negative numbers are not allowed")
+        val biggerSize = maxOf(toString().length, other.toString().length)
+        val smallerSize = minOf(toString().length, other.toString().length)
+        var res = ""
+        var negativeMemory = 0
+        for (i in 1..biggerSize) {
+            var digit: Int = try {
+                getValue(biggerSize - 1).toString()
+                getValue(biggerSize - i) - if (i >= smallerSize + 1) 0 else other.getValue(smallerSize - i)
+            } catch (e: StringIndexOutOfBoundsException) {
+                other.getValue(biggerSize - i) - if (i >= smallerSize + 1) 0 else getValue(smallerSize - i)
+            } + negativeMemory
+            negativeMemory = 0
+            if (digit < 0) {
+                digit += 10
+                negativeMemory = -1
+            }
+            res = "${digit % 10}" + res
+        }
+        return UnsignedBigInteger(res)
+    }
 
     /**
      * Умножение
      */
-    operator fun times(other: UnsignedBigInteger): UnsignedBigInteger = TODO()
+    operator fun times(other: UnsignedBigInteger): UnsignedBigInteger {
+        val biggerSize = maxOf(toString().length, other.toString().length)
+        val smallerSize = minOf(toString().length, other.toString().length)
+        val zeros = Regex("""(0*)$""").find(this.toString())?.value ?: "" +
+        (Regex("""(0*)$""").find(other.toString())?.value ?: "")
+        var res: String
+        var memory = 0
+        val terms = mutableListOf<UnsignedBigInteger>()
+        for (j in 1..smallerSize) {
+            var term = ""
+            for (i in 1..biggerSize) {
+                val digit: Int = try {
+                    getValue(biggerSize - 1).toString()
+                    getValue(biggerSize - i) * other.getValue(smallerSize - j)
+                } catch (e: StringIndexOutOfBoundsException) {
+                    other.getValue(biggerSize - i) * getValue(smallerSize - j)
+                } + memory
+                memory = 0
+                if (digitNumber(digit) > 1) memory += digit / 10
+                term = "${digit % 10}" + term
+            }
+            if (memory != 0) term = "$memory$term"
+            term += "0".repeat(j - 1)
+            memory = 0
+            terms.add(UnsignedBigInteger(term))
+        }
+        var uBIs = UnsignedBigInteger(0)
+        for (t in terms) uBIs += t
+        res = uBIs.toString()
+        res = "$res$zeros"
+        return UnsignedBigInteger(res)
+    }
 
     /**
      * Деление
      */
-    operator fun div(other: UnsignedBigInteger): UnsignedBigInteger = TODO()
+    operator fun div(other: UnsignedBigInteger): UnsignedBigInteger {
+        if (other == UnsignedBigInteger(0)) throw java.lang.ArithmeticException("cannot be divided by zero")
+        if (other > this) return UnsignedBigInteger(0)
+        val biggerSize = maxOf(toString().length, other.toString().length)
+        val smallerSize = minOf(toString().length, other.toString().length)
+        var res = ""
+        var number = toString().substring(0, smallerSize)
+        for (i in smallerSize..biggerSize) {
+            var r = 0
+            if (UnsignedBigInteger(number) < other) {
+                number += if (i != biggerSize) getValue(i).toString() else 0
+                res += "0"
+                continue
+            }
+            while (other * UnsignedBigInteger(r) <= UnsignedBigInteger(number)) r++
+            res += --r
+            number = (UnsignedBigInteger(number) - UnsignedBigInteger(r) * other).toString() +
+                    if (i != biggerSize) getValue(i).toString() else 0
+        }
+        return UnsignedBigInteger(res)
+    }
 
     /**
      * Взятие остатка
      */
-    operator fun rem(other: UnsignedBigInteger): UnsignedBigInteger = TODO()
+    operator fun rem(other: UnsignedBigInteger): UnsignedBigInteger = this - this / other * other
 
     /**
      * Сравнение на равенство (по контракту Any.equals)
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        if (other !is UnsignedBigInteger || toString().length != other.toString().length) return false
+        for (i in 0..toString().lastIndex) if (toString()[i] != other.toString()[i]) return false
+        return true
+    }
 
     /**
      * Сравнение на больше/меньше (по контракту Comparable.compareTo)
      */
-    override fun compareTo(other: UnsignedBigInteger): Int = TODO()
+    override fun compareTo(other: UnsignedBigInteger): Int {
+        if (toString().length > other.toString().length) return 1
+        if (toString().length < other.toString().length) return -1
+        for (i in 0..toString().lastIndex) {
+            if (getValue(i) > other.getValue(i)) return 1
+            if (getValue(i) < other.getValue(i)) return -1
+        }
+        return 0
+    }
 
     /**
      * Преобразование в строку
      */
-    override fun toString(): String = TODO()
+    override fun toString(): String = buildString { for (i in unsignedBigInteger) append(i.toString()) }
 
     /**
      * Преобразование в целое
      * Если число не влезает в диапазон Int, бросить ArithmeticException
      */
-    fun toInt(): Int = TODO()
+    fun toInt(): Int =
+        if (unsignedBigInteger.size > 1) throw ArithmeticException("too big number") else unsignedBigInteger[0]
 
+    /**
+     * Получение цифры по индексу
+     */
+    fun getValue(i: Int): Int = (toString()[i] - '0')
+    override fun hashCode(): Int = unsignedBigInteger.hashCode()
 }
--- test/lesson11/task1/UnsignedBigIntegerTest.kt
+++ test/lesson11/task1/UnsignedBigIntegerTest.kt
@@ -13,6 +13,32 @@ internal class UnsignedBigIntegerTest {
     fun plus() {
         assertEquals(UnsignedBigInteger(4), UnsignedBigInteger(2) + UnsignedBigInteger(2))
         assertEquals(UnsignedBigInteger("9087654330"), UnsignedBigInteger("9087654329") + UnsignedBigInteger(1))
+        assertEquals(UnsignedBigInteger(2), UnsignedBigInteger(2) + UnsignedBigInteger("0000000"))
+        assertEquals(UnsignedBigInteger(90876544), UnsignedBigInteger(1) + UnsignedBigInteger(90876543))
+        assertEquals(
+            UnsignedBigInteger("90876543291230303004"),
+            UnsignedBigInteger("90876543291230303003") + UnsignedBigInteger(1)
+        )
+        assertEquals(
+            UnsignedBigInteger("2147483648000"),
+            UnsignedBigInteger("${Int.MAX_VALUE}999") + UnsignedBigInteger("1")
+        )
+        assertEquals(
+            UnsignedBigInteger("4294967295998"),
+            UnsignedBigInteger("${Int.MAX_VALUE}999") + UnsignedBigInteger("${Int.MAX_VALUE}999")
+        )
+        assertEquals(
+            UnsignedBigInteger("90876543291230303004"),
+            UnsignedBigInteger(1) + UnsignedBigInteger("90876543291230303003")
+        )
+        assertEquals(
+            UnsignedBigInteger("908000000050000000007"),
+            UnsignedBigInteger(1) + UnsignedBigInteger("908000000050000000006")
+        )
+        assertEquals(
+            UnsignedBigInteger("128869304901888001"),
+            UnsignedBigInteger(1) + UnsignedBigInteger("128869304901888000")
+        )
     }
 
     @Test
@@ -20,6 +46,15 @@ internal class UnsignedBigIntegerTest {
     fun minus() {
         assertEquals(UnsignedBigInteger(2), UnsignedBigInteger(4) - UnsignedBigInteger(2))
         assertEquals(UnsignedBigInteger("9087654329"), UnsignedBigInteger("9087654330") - UnsignedBigInteger(1))
+        assertEquals(
+            UnsignedBigInteger("4078814305"),
+            UnsignedBigInteger("12668748897") - UnsignedBigInteger("8589934592")
+        )
+        assertEquals(
+            UnsignedBigInteger(0),
+            UnsignedBigInteger("${Int.MAX_VALUE}999") - UnsignedBigInteger("${Int.MAX_VALUE}999")
+        )
+        assertEquals(UnsignedBigInteger(908764976), UnsignedBigInteger(908765432) - UnsignedBigInteger(456))
         assertThrows(ArithmeticException::class.java) {
             UnsignedBigInteger(2) - UnsignedBigInteger(4)
         }
@@ -30,7 +65,12 @@ internal class UnsignedBigIntegerTest {
     fun times() {
         assertEquals(
             UnsignedBigInteger("18446744073709551616"),
-            UnsignedBigInteger("4294967296‬") * UnsignedBigInteger("4294967296‬")
+            UnsignedBigInteger("4294967296") * UnsignedBigInteger("4294967296")
+        )
+        assertEquals(UnsignedBigInteger(0), UnsignedBigInteger(0) * UnsignedBigInteger("4294967296"))
+        assertEquals(
+            UnsignedBigInteger("2764762067365105152"),
+            UnsignedBigInteger(64362) * UnsignedBigInteger("42956434967296")
         )
     }
 
@@ -38,8 +78,20 @@ internal class UnsignedBigIntegerTest {
     @Tag("16")
     fun div() {
         assertEquals(
-            UnsignedBigInteger("4294967296‬"),
-            UnsignedBigInteger("18446744073709551616") / UnsignedBigInteger("4294967296‬")
+            UnsignedBigInteger("4294967296"),
+            UnsignedBigInteger("18446744073709551616") / UnsignedBigInteger("4294967296")
+        )
+        assertEquals(
+            UnsignedBigInteger(6172839),
+            UnsignedBigInteger(12345678) / UnsignedBigInteger(2)
+        )
+        assertEquals(
+            UnsignedBigInteger(1447),
+            UnsignedBigInteger("123456786518746529") / UnsignedBigInteger("85264963565376")
+        )
+        assertEquals(
+            UnsignedBigInteger(0),
+            UnsignedBigInteger("123456786518746529") / UnsignedBigInteger("8526674674744963565376")
         )
     }
 
@@ -49,7 +101,7 @@ internal class UnsignedBigIntegerTest {
         assertEquals(UnsignedBigInteger(5), UnsignedBigInteger(19) % UnsignedBigInteger(7))
         assertEquals(
             UnsignedBigInteger(0),
-            UnsignedBigInteger("18446744073709551616") % UnsignedBigInteger("4294967296‬")
+            UnsignedBigInteger("18446744073709551616") % UnsignedBigInteger("4294967296")
         )
     }
 
